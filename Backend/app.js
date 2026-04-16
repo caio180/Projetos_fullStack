f430b659418c4f42cdbb27d78f098cee72afd90e
caio180 <caio.lima.viana2009@gmail.com>
2026-04-16 15:10:51 -0300

    Pegando a requisição do usuário da area de cadastro

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,23 @@
 import express from 'express';
 const app = express();
-const porta = 3000
+const porta = 3000;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    
-
-
+    res.send('Servidor funcionando');
 });
 
-app.listen(porta, () => console.log("Servidor rodando  na porta http://localhost:3000"));
+app.post('/cadastro', (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    console.log(nome, email, senha);
+
+    res.json({
+        mensagem: 'Cadastro realizado com sucesso'
+    });
+});
+
+app.listen(porta, () => 
+    console.log("Servidor rodando na porta http://localhost:3000")
+);
